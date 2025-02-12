bf586bb63a0335a3f66818e0b001ad2ab58d9765
Priyansh Verma <priyansh2112@gmail.com>
2025-02-12 23:57:02 +0530

    Added db and completed http endpoints

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv"
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleWare } from "./middleware";
 import { UserSchema, SingInSchema, CreateRoomSchema } from  "@repo/common/config"
+import { prismaClient  } from "@repo/db/client";
 
 
 const app = express();
@@ -12,53 +13,101 @@ dotenv.config();
 
 
 
-app.get("/signup", (req,res)=>{
- 
-    
-    const email = req.body.email;
-    const password = req.body.password ;
+app.get("/signup", async (req,res)=>{
 
-    
-
-    //db call to put this in db
-
-
-    
-    
-    console.log("Hi there");
+    const parsedData = UserSchema.safeParse(req.body);
+    if(!parsedData.success){
+        res.json({
+            message:"Incorrect Inputs "
+        })
+        return;
+    }
+    try{
+        const user = await prismaClient.user.create({
+            data:{
+                email:parsedData.data?.username,
+                password:parsedData.data.password,
+                //hash the pw 
+                name:parsedData.data.name
+            }
+            
+        })
+        res.json({
+            userId:user.id
+        })
+    }catch(e){
+        res.status(411).json({
+            message:"User already exists"
+        })
+    }
 
 })
 
 
-app.post("/signin",(req,res)=>{
-    const data = SingInSchema.safeParse(req.body);
+app.post("/signin", async (req,res)=>{
+    const parsedData = SingInSchema.safeParse(req.body);
 
-    if(!data.success){
+    if(!parsedData.success){
         res.json({
             message: "Incorrect inputs"
         })
         return 
     }
-    //check this in db whether it is present or not and then let them in
-    const userId=1;
-    if(userId){
-        const token = jwt.sign({
-        userId
-    },JWT_SECRET)
+
+    //comapre the hashed pw
+    const user = await prismaClient.user.findFirst({
+        where:{
+            email: parsedData.data.username,
+            password: parsedData.data.password
+        }
+    })
+
+    if(!user){
+        res.status(403).json({
+            message:"Not authorised"
+        })
+        return;
     }
+
+    const token = jwt.sign({
+        userId : user?.id
+    },JWT_SECRET);
+
+    res.json({
+        token
+    })
 
 })
 
-app.get("room", middleWare ,(req,res)=>{
-    const data = CreateRoomSchema.safeParse(req.body);
+app.get("room", middleWare ,async (req,res)=>{
+    const parsedData = CreateRoomSchema.safeParse(req.body);
 
-    if(!data.success){
+    if(!parsedData.success){
         res.json({
             message: "Incorrect inputs"
         })
-        return 
+        return;
     }
     //check this in db whether it is present or not and then let them create a room
+    //@ts-ignore:TODO 
+    const userId = req.userId;
+    try{
+
+        const room = await prismaClient.room.create({
+            data:{
+                slug: parsedData.data.name,
+                adminID : userId
+            }
+        })
+        
+        res.json({
+            roomId : room.id
+        })
+    }catch(e){
+        res.status(411).json({
+            message:"Room already exists"
+        })
+    }
 })
 
 app.listen(3001);
